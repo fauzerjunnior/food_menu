483b93c31bf466cd697c8da875fd6d2f5e5d3acc
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-05-04 13:21:56 -0300

    feat: add header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import Header from "./_components/header";
+
 const Home = () => {
-  return <div className="m-6 bg-red-500 p-5">hello tailwindcss!</div>;
+  return <Header />;
 };
 
 export default Home;
