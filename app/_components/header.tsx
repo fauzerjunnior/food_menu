ed4bdc7e08f57d06c4c7c70508f422ebc1d67869
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-05-04 13:32:56 -0300

    feat: add initial search component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -5,7 +5,7 @@ import { MenuIcon } from "lucide-react";
 const Header = () => {
   return (
     <div className="flex justify-between px-5 pt-24">
-      <Image src="/logo.png" alt="Ifood APP" height={30} width={100} />
+      <Image src="/logo.svg" alt="Ifood APP" height={30} width={100} />
       <Button
         size="icon"
         variant="outline"
