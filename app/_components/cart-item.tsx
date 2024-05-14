40286f94c63caabb7c96e79f4cdc8236f6fa95a5
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-05-14 01:28:08 -0300

    feat: add cart with provider & ui

---
+++ app/_components/cart-item.tsx
@@ -0,0 +1,95 @@
+import Image from "next/image";
+import { CartContext, CartProduct } from "../_context/cart";
+import { calculateProductTotalPrice, formatCurrency } from "../_helpers/price";
+import { Button } from "./ui/button";
+import { ChevronLeftIcon, ChevronRightIcon, TrashIcon } from "lucide-react";
+import { useContext } from "react";
+
+interface CartItem {
+  cartProduct: CartProduct;
+}
+
+const CartItem = ({ cartProduct }: CartItem) => {
+  const {
+    decreaseProductQuantity,
+    increateProductQuantity,
+    removeProductFromCart,
+  } = useContext(CartContext);
+
+  const handleIncreaseProductQuantity = () => {
+    increateProductQuantity(cartProduct.id);
+  };
+
+  const handleDecreaseProductQuantity = () => {
+    decreaseProductQuantity(cartProduct.id);
+  };
+
+  const handleRemoveProductFromCart = () => {
+    removeProductFromCart(cartProduct.id);
+  };
+
+  return (
+    <div className="flex items-center justify-between">
+      <div className="flex gap-5">
+        <div className="relative h-20 w-20">
+          <Image
+            className="rounded-lg object-cover"
+            src={cartProduct.imageUrl}
+            alt={cartProduct.name}
+            fill
+          />
+        </div>
+
+        <div className="space-y-1">
+          <h3 className="text-xs">{cartProduct.name}</h3>
+          <div className="flex items-center gap-1">
+            <h4 className="text-sm font-semibold">
+              {formatCurrency(
+                calculateProductTotalPrice(cartProduct) * cartProduct.quantity,
+              )}
+            </h4>
+            {cartProduct.discountPercentage > 0 && (
+              <span className="text-xs text-muted-foreground line-through">
+                {formatCurrency(
+                  Number(cartProduct.price) * cartProduct.quantity,
+                )}
+              </span>
+            )}
+          </div>
+
+          <div className="flex items-center">
+            <div className="flex items-center gap-3 text-center">
+              <Button
+                size="icon"
+                variant="ghost"
+                className="h-7 w-7 border border-solid border-muted-foreground"
+                onClick={handleDecreaseProductQuantity}
+              >
+                <ChevronLeftIcon size={16} />
+              </Button>
+              <span className="block w-4 text-xs">{cartProduct.quantity}</span>
+              <Button
+                size="icon"
+                className="h-7 w-7"
+                onClick={handleIncreaseProductQuantity}
+              >
+                <ChevronRightIcon size={16} />
+              </Button>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <Button
+        size="icon"
+        variant="ghost"
+        className="h-8 w-8 border border-solid border-muted-foreground"
+        onClick={handleRemoveProductFromCart}
+      >
+        <TrashIcon size={18} />
+      </Button>
+    </div>
+  );
+};
+
+export default CartItem;
---
+++ app/_context/cart.tsx
@@ -0,0 +1,135 @@
+/* eslint-disable no-unused-vars */
+"use client";
+
+import { Prisma, Product } from "@prisma/client";
+import { ReactNode, createContext, useMemo, useState } from "react";
+import { calculateProductTotalPrice } from "../_helpers/price";
+
+export interface CartProduct
+  extends Prisma.ProductGetPayload<{
+    include: {
+      restaurant: {
+        select: {
+          deliveryFee: true;
+        };
+      };
+    };
+  }> {
+  quantity: number;
+}
+
+interface ICartContext {
+  subtotalPrice: number;
+  totalPrice: number;
+  totalDiscounts: number;
+  products: CartProduct[];
+  addProductToCart: (products: Product, quantity: number) => void;
+  decreaseProductQuantity: (productId: string) => void;
+  increateProductQuantity: (productId: string) => void;
+  removeProductFromCart: (productId: string) => void;
+}
+
+export const CartContext = createContext<ICartContext>({
+  products: [],
+  subtotalPrice: 0,
+  totalPrice: 0,
+  totalDiscounts: 0,
+  addProductToCart: () => {},
+  decreaseProductQuantity: () => {},
+  increateProductQuantity: () => {},
+  removeProductFromCart: () => {},
+});
+
+export const CartProvider = ({ children }: { children: ReactNode }) => {
+  const [products, setProducts] = useState<CartProduct[]>([]);
+
+  const subtotalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + Number(product.price) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalPrice = useMemo(() => {
+    return products.reduce((acc, product) => {
+      return acc + calculateProductTotalPrice(product) * product.quantity;
+    }, 0);
+  }, [products]);
+
+  const totalDiscounts = subtotalPrice - totalPrice;
+
+  const addProductToCart = (product: Product, quantity: number) => {
+    const isProductAlreadyInCart = products.some(
+      (cartProduct) => cartProduct.id === product.id,
+    );
+
+    if (isProductAlreadyInCart) {
+      return setProducts((current) =>
+        current.map((cartProduct) => {
+          if (cartProduct.id === product.id) {
+            return {
+              ...cartProduct,
+              quantity: cartProduct.quantity + quantity,
+            };
+          }
+
+          return cartProduct;
+        }),
+      );
+    }
+
+    setProducts((current) => [...current, { ...product, quantity: quantity }]);
+  };
+
+  const decreaseProductQuantity = (productId: string) => {
+    return setProducts((current) =>
+      current.map((cartProduct) => {
+        if (cartProduct.id === productId && cartProduct.quantity > 1) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity - 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const increateProductQuantity = (productId: string) => {
+    return setProducts((current) =>
+      current.map((cartProduct) => {
+        if (cartProduct.id === productId) {
+          return {
+            ...cartProduct,
+            quantity: cartProduct.quantity + 1,
+          };
+        }
+
+        return cartProduct;
+      }),
+    );
+  };
+
+  const removeProductFromCart = (productId: string) => {
+    return setProducts((products) =>
+      products.filter((cartProduct) => cartProduct.id !== productId),
+    );
+  };
+
+  return (
+    <CartContext.Provider
+      value={{
+        products,
+        subtotalPrice,
+        totalPrice,
+        totalDiscounts,
+        addProductToCart,
+        decreaseProductQuantity,
+        increateProductQuantity,
+        removeProductFromCart,
+      }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+};
