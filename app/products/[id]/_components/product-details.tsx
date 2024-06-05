e985a776c124937438961720d0d30c5fe311c1ac
Fauzer Junior <fauzerjunnior@hotmail.com>
2024-06-04 23:45:57 -0300

    feat: add prop to close cart sheet on order create

--- app/products/[id]/_components/product-details.tsx
+++ app/products/[id]/_components/product-details.tsx
@@ -160,7 +160,7 @@ const ProductDetails = ({ product, relatedProducts }: ProductDetailsProps) => {
           <SheetHeader>
             <SheetTitle className="text-left">Sacola</SheetTitle>
           </SheetHeader>
-          <Cart />
+          <Cart setIsOpen={setIsCartOpen} />
         </SheetContent>
       </Sheet>
 
